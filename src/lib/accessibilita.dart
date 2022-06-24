77d2e817b0ef3a2573d772f01075910f4c1e6968
Everysimo <simone.spera.13@gmail.com>
2022-06-24 19:48:25 +0200

    fix: accessibility full working

--- src/lib/accessibilita.dart
+++ src/lib/accessibilita.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:travelty/luogo.dart';
+import 'package:travelty/recensioni_accessibilita.dart';
+import 'package:travelty/utente.dart';
 
 class AccessibilitaPage extends StatelessWidget {
-  final Function nextPage;
-  final Function previewPage;
-  final String nomeLuogo;
-  const AccessibilitaPage(
-      {Key? key,
-      required this.nextPage,
-      required this.previewPage,
-      required this.nomeLuogo})
-      : super(key: key);
+  final int indexLuogo;
+  const AccessibilitaPage({
+    Key? key,
+    required this.indexLuogo,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +16,7 @@ class AccessibilitaPage extends StatelessWidget {
       appBar: AppBar(
         leading: IconButton(
           onPressed: () {
-            previewPage();
+            Navigator.pop(context);
           },
           icon: const Icon(
             Icons.arrow_back,
@@ -30,7 +29,7 @@ class AccessibilitaPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                nomeLuogo,
+                luoghi[indexLuogo].nome,
                 style: const TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
@@ -61,85 +60,131 @@ class AccessibilitaPage extends StatelessWidget {
           ),
         ],
       ),
-      body: const Accessibilita(),
+      body: Accessibilita(
+        indexLuogo: indexLuogo,
+      ),
     );
   }
 }
 
 class Accessibilita extends StatefulWidget {
-  const Accessibilita({Key? key}) : super(key: key);
+  final int indexLuogo;
+  const Accessibilita({Key? key, required this.indexLuogo}) : super(key: key);
 
   @override
   State<Accessibilita> createState() => _AccessibilitaState();
 }
 
 class _AccessibilitaState extends State<Accessibilita> {
+  late int accessibilita = 1;
+  late String recensione;
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        const AccessibilitaForm(),
-        for (int i = 0; i < 20; i++)
-          AccessibilitaItem(
-            key: Key("Account$i"),
+        Form(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 5),
+            color: const Color(0XFFF4F4F4),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: const CircleAvatar(
+                    backgroundColor: Color(0XFFB9B9B9),
+                    child: Icon(Icons.person),
+                  ),
+                  title: Material(
+                    elevation: 13,
+                    shadowColor: Colors.grey,
+                    borderRadius: BorderRadius.circular(15),
+                    child: TextFormField(
+                      onChanged: (value) {
+                        recensione = value;
+                      },
+                      onFieldSubmitted: (value) {
+                        setState(() {
+                          if (luoghi[widget.indexLuogo].mediaPericolo == 0) {
+                            luoghi[widget.indexLuogo].mediaAccessibilita =
+                                accessibilita;
+                          } else {
+                            luoghi[widget.indexLuogo].mediaPericolo =
+                                (luoghi[widget.indexLuogo].mediaAccessibilita +
+                                        accessibilita / 2)
+                                    .toInt();
+                          }
+                          luoghi[widget.indexLuogo].accessibilita.add(
+                                RecensioneAccessibilita(
+                                  utenteLoggato,
+                                  0,
+                                  recensione,
+                                  [],
+                                  accessibilita,
+                                ),
+                              );
+                          luoghi[widget.indexLuogo].accessibilita.sort(
+                                (a, b) => b.voto.compareTo(a.voto),
+                              );
+                        });
+                      },
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.newline,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        suffixIcon: SelectAccessibilitaLevel(
+                          onColor: (color) {
+                            if (color == Colors.green) {
+                              accessibilita = 1;
+                            } else if (color == Colors.orange) {
+                              accessibilita = 2;
+                            } else if (color == Colors.red) {
+                              accessibilita = 3;
+                            } else {
+                              accessibilita = -1;
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+        ),
+        ...List.generate(
+          luoghi[widget.indexLuogo].accessibilita.length,
+          (index) => AccessibilitaItem(
+            key: Key("Accessibilita${widget.indexLuogo}$index"),
+            indexLuogo: widget.indexLuogo,
+            indexAccessibilita: index,
+            onVote: () {
+              setState(() {
+                luoghi[widget.indexLuogo].accessibilita.sort(
+                      (a, b) => b.voto.compareTo(a.voto),
+                    );
+              });
+            },
+          ),
+        )
       ],
     );
   }
 }
 
-class AccessibilitaForm extends StatelessWidget {
-  const AccessibilitaForm({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 5),
-        color: const Color(0XFFF4F4F4),
-        child: Column(
-          children: [
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Color(0XFFB9B9B9),
-                child: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
-              ),
-              title: Material(
-                elevation: 13,
-                shadowColor: Colors.grey,
-                borderRadius: BorderRadius.circular(15),
-                child: TextFormField(
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.newline,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(15),
-                      ),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    suffixIcon: SelectAccessibilitaLevel(),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class SelectAccessibilitaLevel extends StatefulWidget {
-  const SelectAccessibilitaLevel({Key? key}) : super(key: key);
+  final Function onColor;
+  const SelectAccessibilitaLevel({Key? key, required this.onColor})
+      : super(key: key);
 
   @override
   State<SelectAccessibilitaLevel> createState() =>
@@ -153,6 +198,7 @@ class _SelectAccessibilitaLevelState extends State<SelectAccessibilitaLevel> {
     setState(() {
       selectColor = newColor;
     });
+    widget.onColor(selectColor);
   }
 
   @override
@@ -168,26 +214,32 @@ class _SelectAccessibilitaLevelState extends State<SelectAccessibilitaLevel> {
       itemBuilder: (context) => [
         const PopupMenuItem(
           value: Colors.red,
-          child: Icon(
-            Icons.accessible_outlined,
-            color: Colors.red,
-            size: 40,
+          child: Center(
+            child: Icon(
+              Icons.accessible_outlined,
+              color: Colors.red,
+              size: 40,
+            ),
           ),
         ),
         const PopupMenuItem(
           value: Colors.orange,
-          child: Icon(
-            Icons.accessible_outlined,
-            color: Colors.orange,
-            size: 40,
+          child: Center(
+            child: Icon(
+              Icons.accessible_outlined,
+              color: Colors.orange,
+              size: 40,
+            ),
           ),
         ),
         const PopupMenuItem(
           value: Colors.green,
-          child: Icon(
-            Icons.accessible_outlined,
-            color: Colors.green,
-            size: 40,
+          child: Center(
+            child: Icon(
+              Icons.accessible_outlined,
+              color: Colors.green,
+              size: 40,
+            ),
           ),
         )
       ],
@@ -196,27 +248,21 @@ class _SelectAccessibilitaLevelState extends State<SelectAccessibilitaLevel> {
 }
 
 class AccessibilitaItem extends StatefulWidget {
-  const AccessibilitaItem({Key? key}) : super(key: key);
+  final int indexLuogo;
+  final int indexAccessibilita;
+  final Function onVote;
+  const AccessibilitaItem({
+    Key? key,
+    required this.indexLuogo,
+    required this.indexAccessibilita,
+    required this.onVote,
+  }) : super(key: key);
 
   @override
   State<AccessibilitaItem> createState() => _AccessibilitaItemState();
 }
 
 class _AccessibilitaItemState extends State<AccessibilitaItem> {
-  int vote = 0;
-
-  upVote() {
-    setState(() {
-      vote = vote + 1;
-    });
-  }
-
-  downVote() {
-    setState(() {
-      vote = vote - 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Flex(
@@ -227,16 +273,41 @@ class _AccessibilitaItemState extends State<AccessibilitaItem> {
           children: [
             IconButton(
               onPressed: () {
-                upVote();
+                luoghi[widget.indexLuogo]
+                    .accessibilita[widget.indexAccessibilita]
+                    .upVote(utenteLoggato);
+                widget.onVote();
               },
-              icon: const Icon(Icons.arrow_drop_up),
+              icon: Icon(
+                Icons.arrow_drop_up,
+                color: luoghi[widget.indexLuogo]
+                            .accessibilita[widget.indexAccessibilita]
+                            .getVoto(utenteLoggato) ==
+                        1
+                    ? Colors.green
+                    : Colors.black,
+              ),
             ),
-            Text(vote.toString()),
+            Text(luoghi[widget.indexLuogo]
+                .accessibilita[widget.indexAccessibilita]
+                .voto
+                .toString()),
             IconButton(
               onPressed: () {
-                downVote();
+                luoghi[widget.indexLuogo]
+                    .accessibilita[widget.indexAccessibilita]
+                    .downVoto(utenteLoggato);
+                widget.onVote();
               },
-              icon: const Icon(Icons.arrow_drop_down),
+              icon: Icon(
+                Icons.arrow_drop_down,
+                color: luoghi[widget.indexLuogo]
+                            .accessibilita[widget.indexAccessibilita]
+                            .getVoto(utenteLoggato) ==
+                        -1
+                    ? Colors.red
+                    : Colors.black,
+              ),
             ),
           ],
         ),
@@ -250,17 +321,34 @@ class _AccessibilitaItemState extends State<AccessibilitaItem> {
                 children: [
                   Flex(
                     direction: Axis.horizontal,
-                    children: const [
-                      CircleAvatar(
-                        radius: 12,
-                        child: Icon(
-                          Icons.person,
-                          size: 15,
-                        ),
+                    children: [
+                      Icon(
+                        Icons.workspace_premium,
+                        color: luoghi[widget.indexLuogo]
+                                    .accessibilita[widget.indexAccessibilita]
+                                    .utente
+                                    .rank ==
+                                1
+                            ? const Color(0XFFD27C2C)
+                            : luoghi[widget.indexLuogo]
+                                        .accessibilita[
+                                            widget.indexAccessibilita]
+                                        .utente
+                                        .rank ==
+                                    2
+                                ? const Color(0XFFC4C4C4)
+                                : luoghi[widget.indexLuogo]
+                                            .accessibilita[
+                                                widget.indexAccessibilita]
+                                            .utente
+                                            .rank ==
+                                        3
+                                    ? const Color(0XFFFFC700)
+                                    : Colors.black,
                       ),
                       Text(
-                        "Utente x",
-                        style: TextStyle(
+                        "${luoghi[widget.indexLuogo].accessibilita[widget.indexAccessibilita].utente.nome} ${luoghi[widget.indexLuogo].accessibilita[widget.indexAccessibilita].utente.cognome}",
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
@@ -268,15 +356,32 @@ class _AccessibilitaItemState extends State<AccessibilitaItem> {
                   ),
                 ],
               ),
-              const Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+              Text(
+                luoghi[widget.indexLuogo]
+                    .accessibilita[widget.indexAccessibilita]
+                    .informazioni,
               ),
             ],
           ),
         ),
-        const Icon(
+        Icon(
           Icons.accessible_outlined,
-          color: Colors.orange,
+          color: luoghi[widget.indexLuogo]
+                      .accessibilita[widget.indexAccessibilita]
+                      .accessibilita ==
+                  1
+              ? Colors.green
+              : luoghi[widget.indexLuogo]
+                          .accessibilita[widget.indexAccessibilita]
+                          .accessibilita ==
+                      2
+                  ? Colors.orange
+                  : luoghi[widget.indexLuogo]
+                              .accessibilita[widget.indexAccessibilita]
+                              .accessibilita ==
+                          3
+                      ? Colors.red
+                      : Colors.grey,
           size: 50,
         )
       ],

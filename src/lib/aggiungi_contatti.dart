4a4e0a1a3be1fafd63969fbf517625df8d0ab3cb
Everysimo <simone.spera.13@gmail.com>
2022-06-24 19:45:30 +0200

    fix: add contacts full working

--- src/lib/aggiungi_contatti.dart
+++ src/lib/aggiungi_contatti.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:travelty/contatti.dart';
+import 'package:travelty/contatto.dart';
+import 'package:travelty/luogo.dart';
+import 'package:travelty/main.dart';
+import 'package:travelty/utente.dart';
 
 class AggiungiContatti extends StatelessWidget {
-  final Function nextPage;
-  final Function previewPage;
-  final String nomeLuogo;
-  const AggiungiContatti(
-      {Key? key,
-      required this.nextPage,
-      required this.previewPage,
-      required this.nomeLuogo})
-      : super(key: key);
+  final int indexLuogo;
+  late String tipo;
+  late String contatto;
+  late String info;
+  AggiungiContatti({
+    Key? key,
+    required this.indexLuogo,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           onPressed: () {
-            previewPage();
+            Navigator.pop(context);
           },
           icon: const Icon(
             Icons.arrow_back,
@@ -24,11 +28,27 @@ class AggiungiContatti extends StatelessWidget {
           ),
         ),
         backgroundColor: const Color(0XFF5BA942),
-        title: const Text(
-          "Aggiungi contatti",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+        title: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                luoghi[indexLuogo].nome,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const Text(
+                "Aggiungi contatti",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
+              ),
+            ],
           ),
         ),
         actions: const [
@@ -55,6 +75,9 @@ class AggiungiContatti extends StatelessWidget {
                 shadowColor: Colors.grey,
                 borderRadius: BorderRadius.circular(15),
                 child: TextFormField(
+                  onChanged: (value) {
+                    tipo = value;
+                  },
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.newline,
                   decoration: const InputDecoration(
@@ -81,6 +104,9 @@ class AggiungiContatti extends StatelessWidget {
                 shadowColor: Colors.grey,
                 borderRadius: BorderRadius.circular(15),
                 child: TextFormField(
+                  onChanged: (value) {
+                    contatto = value;
+                  },
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.newline,
                   decoration: const InputDecoration(
@@ -107,6 +133,9 @@ class AggiungiContatti extends StatelessWidget {
                 shadowColor: Colors.grey,
                 borderRadius: BorderRadius.circular(15),
                 child: TextFormField(
+                  onChanged: (value) {
+                    info = value;
+                  },
                   textInputAction: TextInputAction.newline,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(
@@ -129,16 +158,44 @@ class AggiungiContatti extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color(0XFF4C8F38),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  "Conferma",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
+              child: SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0XFF4C8F38),
+                  ),
+                  onPressed: () {
+                    luoghi[indexLuogo].contatti.add(
+                          Contatto(
+                            utenteLoggato,
+                            0,
+                            info,
+                            [],
+                            tipo,
+                            contatto,
+                          ),
+                        );
+                    luoghi[indexLuogo].contatti.sort(
+                          (a, b) => b.voto.compareTo(a.voto),
+                        );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MyPage(
+                          child: ContattiPage(
+                            indexLuogo: indexLuogo,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Conferma",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
                   ),
                 ),
               ),

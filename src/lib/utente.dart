d8ff45cf212ce687e9fd07c6da9cff48bf4ac1f5
dany98sa <56975152+dany98sa@users.noreply.github.com>
2022-06-24 19:53:56 +0200

    feat: classe utente

--- src/lib/utente.dart
+++ src/lib/utente.dart
@@ -20,6 +20,15 @@ class Utente {
 
 var utenti = [
   Utente(
+    "Utente",
+    "Test",
+    "01/01/2000",
+    "utente.test@dany98.it",
+    "Password123.",
+    3,
+    250,
+  ),
+  Utente(
     "Simone",
     "Spera",
     "28/10/2000",
@@ -28,4 +37,24 @@ var utenti = [
     3,
     250,
   ),
+  Utente(
+    "Daniele",
+    "De Marco",
+    "14/10/1998",
+    "demarcodaniele98@gmail.com",
+    "Password123.",
+    2,
+    150,
+  ),
+  Utente(
+    "Angelo",
+    "Coralluzzo",
+    "28/10/2000",
+    "angelo.coralluzzo@gmail.com",
+    "Password123.",
+    1,
+    50,
+  ),
 ];
+
+var utenteLoggato = utenti[0];
